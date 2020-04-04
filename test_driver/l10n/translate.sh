0e26eb324b82686b938aa88df090022a7b354f45
Kenton Hamaluik <kenton@hamaluik.ca>
2020-04-04 01:10:42 -0600

    normalized translations, added markers for hand translations

--- test_driver/l10n/translate.sh
+++ test_driver/l10n/translate.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+tt -c ../../translation_credentials.json -f en.flt -o . -l ar
 tt -c ../../translation_credentials.json -f en.flt -o . -l de
 tt -c ../../translation_credentials.json -f en.flt -o . -l es
 tt -c ../../translation_credentials.json -f en.flt -o . -l fr
 tt -c ../../translation_credentials.json -f en.flt -o . -l hi
 tt -c ../../translation_credentials.json -f en.flt -o . -l id
+tt -c ../../translation_credentials.json -f en.flt -o . -l it
 tt -c ../../translation_credentials.json -f en.flt -o . -l ja
 tt -c ../../translation_credentials.json -f en.flt -o . -l ko
 tt -c ../../translation_credentials.json -f en.flt -o . -l pt
 tt -c ../../translation_credentials.json -f en.flt -o . -l ru
 tt -c ../../translation_credentials.json -f en.flt -o . -l zh-CN
 tt -c ../../translation_credentials.json -f en.flt -o . -l zh-TW
-tt -c ../../translation_credentials.json -f en.flt -o . -l ar
