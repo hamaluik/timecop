433dd460bcd6e61adbc4bbc06c2a00e7d2f7bf88
Miroslav Mazel <contact.mirek@protonmail.com>
2023-06-04 12:16:19 +0200

    Fixing integration test

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,16 +1,18 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 import 'package:integration_test/integration_test_driver_extended.dart';
 
 Future<void> main() async {
   try {
     await integrationDriver(
-      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-        final File image = await File('screenshots/$screenshotName.png').create(recursive: true);
+      onScreenshot: (String screenshotName, List<int> screenshotBytes,
+          [args]) async {
+        final File image = await File('screenshots/$screenshotName.png')
+            .create(recursive: true);
         image.writeAsBytesSync(screenshotBytes);
         return true;
       },
     );
-  } catch (e) {
-    
-  }
+  } catch (e) {}
 }
