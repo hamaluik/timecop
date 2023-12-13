9638508237fae185fdd0a1a11dd8a92c3561f00f
Miroslav Mazel <contact.mirek+git@protonmail.com>
2023-12-13 22:01:03 +0100

    Make theme work for expanded FABs too

--- lib/themes.dart
+++ lib/themes.dart
@@ -127,7 +127,7 @@ class ThemeUtil {
           dividerTheme:
               DividerThemeData(color: colors.onBackground.withAlpha(31)),
           floatingActionButtonTheme: FloatingActionButtonThemeData(
-              shape: const CircleBorder(),
+              shape: StadiumBorder(),
               backgroundColor: colors.primary,
               foregroundColor: colors.onPrimary),
           textButtonTheme: TextButtonThemeData(
